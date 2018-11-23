e0fbe796c0669df2fe5c54fdec88e195bd2b1232
WuliTT520 <mlh32356516535>
2018-11-23 11:30:55 +0800

    新的activity

---
+++ app/src/main/java/com/zhihui/imeeting/cloudmeeting/activity/SetPasswordActivity.java
@@ -0,0 +1,15 @@
+package com.zhihui.imeeting.cloudmeeting.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhihui.imeeting.cloudmeeting.R;
+
+public class SetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_password);
+    }
+}
