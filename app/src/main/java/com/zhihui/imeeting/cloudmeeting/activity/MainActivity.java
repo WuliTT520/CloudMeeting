4ffba65c2009578377dad6e49681dee6dc0fa7c8
Flzjler <flzjler@gmail.com>
2018-11-23 18:01:20 +0800

    权限获取SDK激活

--- app/src/main/java/com/zhihui/imeeting/cloudmeeting/activity/MainActivity.java
+++ app/src/main/java/com/zhihui/imeeting/cloudmeeting/activity/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -28,20 +27,13 @@ public class MainActivity extends AppCompatActivity {
     ImageView home_pic,schedule_pic,add_pic,news_pic,mine_pic;
     TextView home_text,schedule_text,add_text,news_text,mine_text;
 
-    /**
-     * 所需权限
-     */
-    private static final int ACTION_REQUEST_PERMISSIONS = 0x001;
-    private static final String[] NEEDED_PERMISSIONS = new String[]{
-            Manifest.permission.READ_PHONE_STATE
-    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        activeEngine();
         setListener();
     }
     public void init(){
@@ -168,69 +160,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * 检查权限是否获取
-     * @param neededPermissions
-     * @return
-     */
-    private boolean checkPermissions(String[] neededPermissions) {
-        if (neededPermissions == null || neededPermissions.length == 0) {
-            return true;
-        }
-        boolean allGranted = true;
-        for (String neededPermission : neededPermissions) {
-            allGranted &= ContextCompat.checkSelfPermission(this, neededPermission) == PackageManager.PERMISSION_GRANTED;
-        }
-        return allGranted;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == ACTION_REQUEST_PERMISSIONS) {
-            boolean isAllGranted = true;
-            for (int grantResult : grantResults) {
-                isAllGranted &= (grantResult == PackageManager.PERMISSION_GRANTED);
-            }
-            if (isAllGranted) {
-                activeEngine();
-            } else {
-                showToast("未授权权限");
-            }
-        }
-    }
-
-    /**
-     * 激活引擎
-     */
-    void activeEngine() {
-        if (!checkPermissions(NEEDED_PERMISSIONS)) {
-            ActivityCompat.requestPermissions(this, NEEDED_PERMISSIONS, ACTION_REQUEST_PERMISSIONS);
-            Log.i("j", "进入激活引擎方法");
-            return;
-        }
-        FaceEngine faceEngine = new FaceEngine();
-        int errorCode = faceEngine.active(this, Constants.ArcFace_APP_ID, Constants.ArcFace_SDK_KEY);
-        if(errorCode == ErrorInfo.MOK) {
-            showToast("SDK激活成功");
-        } else if(errorCode == ErrorInfo.MERR_ASF_ALREADY_ACTIVATED) {
-            showToast("SDK已激活");
-        } else {
-            showToast("激活失败, errorCode: " + errorCode);
-        }
-    }
-
-    /**
-     * 显示信息
-     */
-    private Toast toast = null;
-    private void showToast(String s) {
-        if (toast == null) {
-            toast = Toast.makeText(this, s, Toast.LENGTH_SHORT);
-            toast.show();
-        } else {
-            toast.setText(s);
-            toast.show();
-        }
-    }
 }
